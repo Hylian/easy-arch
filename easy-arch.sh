f4578e37b0b0ed8dabefed897492e0e4e9da1e53
Edward Shin <contact@edwardsh.in>
2025-02-27 00:40:42 -0500

    no pacstrap errors

--- easy-arch.sh
+++ easy-arch.sh
@@ -364,7 +364,7 @@ microcode_detector
 
 # Pacstrap (setting up a base sytem onto the new root).
 info_print "Installing the base system (it may take a while)."
-pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs mesa rsync efibootmgr snapper refind reflector snap-pac zram-generator sudo &>/dev/null
+pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs mesa rsync efibootmgr snapper refind reflector snap-pac zram-generator sudo
 
 # Setting up the hostname.
 echo "$hostname" > /mnt/etc/hostname
